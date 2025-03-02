04b38339aed92a4198e9dffae6385e798f3b372b
Billy Moore <billyedmoore@yahoo.com>
2025-03-02 15:00:22 +0000

    Added tests for list_keys.

--- test/dbTest.ml
+++ test/dbTest.ml
@@ -31,6 +31,51 @@ let test_delete_from_db _ =
         | Ok _ ->
             failwith "Get was successful after key was removed (not ideal)." ) )
 
+let test_list_keys_from_db _ =
+  match BarrelDB.Db.create "test_list_keys_from_db" with
+  | Error err ->
+      failwith (BarrelDB.Db.err_to_string err)
+  | Ok db_session -> (
+      ignore (BarrelDB.Db.put db_session "key0" "val0") ;
+      ignore (BarrelDB.Db.put db_session "key1" "val1") ;
+      ignore (BarrelDB.Db.put db_session "key2" "val2") ;
+      ignore (BarrelDB.Db.put db_session "key3" "val3") ;
+      ignore (BarrelDB.Db.put db_session "key4" "val4") ;
+      ignore (BarrelDB.Db.put db_session "key5" "val5") ;
+      ignore (BarrelDB.Db.put db_session "key6" "val6") ;
+      ignore (BarrelDB.Db.put db_session "key7" "val7") ;
+      match BarrelDB.Db.list_keys db_session with
+      | Error err ->
+          failwith (BarrelDB.Db.err_to_string err)
+      | Ok value ->
+          assert_equal
+            ["key0"; "key1"; "key2"; "key3"; "key4"; "key5"; "key6"; "key7"]
+            (List.sort String.compare value) )
+
+let test_list_keys_with_keys_removed_db _ =
+  match BarrelDB.Db.create "test_list_keys_from_db_with_some_keys_removed" with
+  | Error err ->
+      failwith (BarrelDB.Db.err_to_string err)
+  | Ok db_session -> (
+      ignore (BarrelDB.Db.put db_session "key0" "val0") ;
+      ignore (BarrelDB.Db.put db_session "key1" "val1") ;
+      ignore (BarrelDB.Db.put db_session "key2" "val2") ;
+      ignore (BarrelDB.Db.delete db_session "key0") ;
+      ignore (BarrelDB.Db.delete db_session "key1") ;
+      ignore (BarrelDB.Db.delete db_session "key2") ;
+      ignore (BarrelDB.Db.put db_session "key3" "val3") ;
+      ignore (BarrelDB.Db.put db_session "key4" "val4") ;
+      ignore (BarrelDB.Db.put db_session "key5" "val5") ;
+      ignore (BarrelDB.Db.put db_session "key6" "val6") ;
+      ignore (BarrelDB.Db.put db_session "key7" "val7") ;
+      match BarrelDB.Db.list_keys db_session with
+      | Error err ->
+          failwith (BarrelDB.Db.err_to_string err)
+      | Ok value ->
+          assert_equal
+            ["key3"; "key4"; "key5"; "key6"; "key7"]
+            (List.sort String.compare value) )
+
 let test_get_long_value_from_db _ =
   let known_value =
     "fsajdkldsjlajlsdjflajsdklasdkflasdjflasdksdklajsdfljfsldkfajklsdjflasjsljsdflkajsdfalsdfjals"
--- test/test.ml
+++ test/test.ml
@@ -9,6 +9,9 @@ let suite =
          >:: BinaryUtilsTest.test_bytes_of_int_max_value
        ; "test_db_get_long_value" >:: DbTest.test_get_long_value_from_db
        ; "test_db_get" >:: DbTest.test_get_from_db
-       ; "test_db_delete" >:: DbTest.test_delete_from_db ]
+       ; "test_db_delete" >:: DbTest.test_delete_from_db
+       ; "test_db_list_keys" >:: DbTest.test_list_keys_from_db
+       ; "test_db_list_keys_with_removed_keys"
+         >:: DbTest.test_list_keys_with_keys_removed_db ]
 
 let () = run_test_tt_main suite
